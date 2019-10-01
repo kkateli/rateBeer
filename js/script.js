3616991f7c8590730f533a3baa9d3101e679ee69
kkateli <kateli24@outlook.com>
2019-10-01 22:26:46 +1300

    Recorded rates for three types of beer

--- js/script.js
+++ js/script.js
@@ -1,2 +1,105 @@
 "use strict";
+const state ={
+    beer1:"",
+    beer2:"",
+    beer3:""
+}
+
+const beerOneRating = one => {
+state.beer1 = one.value;
+
+};
+const beerTwoRating = two => {
+state.beer2 = two.value;
+};
+
+const beerThreeRating = three => {
+state.beer3 = three.value;
+
+};
+
+const calculator=()=>{
+    const beerOne = {
+        poor :0,
+        fair :0,
+        ok:0,
+        good :0,
+        must :0
+    }
+    const beerTwo = {
+        poor :0,
+        fair :0,
+        ok:0,
+        good :0,
+        must :0
+    }
+    const beerThree = {
+        poor :0,
+        fair :0,
+        ok :0,
+        good :0,
+        must :0
+    }
+        
+        switch(state.beer1){
+            case "beer1-one":
+                beerOne.poor++;
+                break;
+            case "beer1-two":
+                beerOne.fair++;
+                break;
+            case "beer1-three":
+                beerOne.ok++;
+                break;
+            case "beer1-four":
+                beerOne.good++;
+                break;
+            default:
+                beerOne.must++;
+        
+        }
+        console.log(beerOne);
+
+        
+        
+        switch(state.beer2){
+            case "beer2-one":
+                beerTwo.poor++;
+                break;
+            case "beer2-two":
+                beerTwo.fair++;
+                break;
+            case "beer2-three":
+                beerTwo.ok++;
+                break;
+            case "beer2-four":
+                beerTwo.good++;
+                break;
+            default:
+                beerTwo.must++;
+        
+        }
+        console.log(beerTwo);
+
+        
+        
+        switch(state.beer3){
+            case "beer3-one":
+                beerThree.poor++;
+                break;
+            case "beer3-two":
+                beerThree.fair++;
+                break;
+            case "beer3-three":
+                beerThree.ok++;
+                break;
+            case "beer3-four":
+                beerThree.good++;
+                break;
+            default:
+                beerThree.must++;
+        
+        }
+        console.log(beerThree);
+}
 
